55d9599f2cc13f394110173ad25237429b3fae3c
Swiderski, Justin <jswiderski@gatech.edu>
2022-08-23 20:24:54 -0400

    Changed approach and solved

--- wk1/fibonacci.py
+++ wk1/fibonacci.py
@@ -1,21 +1,40 @@
 """
-The goal of this algorithm is to compute the nth number of the Fibonacci sequence
-*starting from 1, instead of 0
+The goal of this algorithm is to compute the nth number of the Fibonacci sequence with Dynamic Programming
+*taking the first two numbers as 0 and 1
+Fn = Fn-1 + Fn-2
 """
 
-def get_int():
-    n = input("This recursion algorithm will accept a positive integer as the nth number in the Fibonacci sequence. ")
-    try:
-        n = int(n)
-    except:
-        n = get_int()
+stored_fibonacci_array = [0, 1]
 
-    if n < 0:
+
+def get_int():
+    nth = int(input("Will accept a positive integer: \n"))
+    if nth < 0:
+        print("C'mon, let's not be annoying. ")
         get_int()
 
-    return n
+    elif nth == 0:
+        return 0
 
-"def fib(n):"
+    elif nth == 1 or nth == 2:
+        return 1
 
-if __name__=="__main__":
+    else:
+        return nth
+
+
+def fibonacci(n=None):
+    if n < len(stored_fibonacci_array):
+        return stored_fibonacci_array[n]
+
+    else:
+        temp = fibonacci(n-1) + fibonacci(n-2)
+        stored_fibonacci_array.append(temp)
+        return stored_fibonacci_array[n]
+
+
+if __name__ == "__main__":
     print("Fibonacci ")
+    num = get_int()
+    nth_fibonacci_number = fibonacci(num)
+    print("Position " + str(num) + " in the Fibonacci sequence is " + str(nth_fibonacci_number) + ".")
